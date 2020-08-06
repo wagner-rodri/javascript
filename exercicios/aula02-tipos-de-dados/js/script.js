86953da1404b5c4e7264abf4f3c0b31497920f41
wagner-rodri <luan.frra@gmail.com>
2020-08-06 16:00:12 -0300

    Aula02 - Tipos de dados

---
+++ exercicios/aula02-tipos-de-dados/js/script.js
@@ -0,0 +1,25 @@
+//Declare uma variável contendo uma string
+var nome = 'Wagner'
+console.log(nome)
+
+//Declare uma variável contendo um número dentro de uma string
+var idade = '28'
+console.log(`O ${nome} tem ${idade} anos de idade.`)
+
+//Declare uma variável contendo sua idade
+var idade = 28
+console.log(idade)
+
+//Declare duas variáveis, uma con seu primeiro nome e outra com seu segundo nome, depois concatene.
+var primeiroNome = 'Wagner'
+var sobreNome = 'Rodrigues'
+var nomeCompleto = `${primeiroNome} ${sobreNome}`
+console.log(nomeCompleto)
+
+//Coloque a seguinte frase em uma variável: It's time
+var frase = 'It\'s time'
+console.log(frase)
+
+//Verifique o tipo de variável que contém o seu nome
+var nome = 'Wagner'
+console.log(typeof nome) 
