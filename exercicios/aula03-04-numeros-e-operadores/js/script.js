03c332c786857d91778365805254ca2093fa4b5e
wagner-rodri <luan.frra@gmail.com>
2020-08-06 18:31:14 -0300

    aula03-04 Números e Operadores

---
+++ exercicios/aula03-04-numeros-e-operadores/js/script.js
@@ -0,0 +1,28 @@
+//Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+
+//Crie duas expressões que tornem resultado NaN
+var totalMacas = '5 maças' - 2
+var totalBananas = '5 bananas' / 12
+console.log(totalMacas, totalBananas)
+
+//Somar a string '200' com o número 50 e retornar o número 250
+var a = '200'
+var b = 50
+var total = +a + b
+console.log(total)
+
+//Incremente o númeor 5 e retorne o seu valor incrementado
+
+var numero = 5
+console.log(++numero)
+
+//Como dividir o peso por 2
+var numero = 80
+var unidadeDeMedida = 'kg'
+var peso = numero+unidadeDeMedida
+console.log(peso)
+var numero = (80 / 2)
+var peso = numero+unidadeDeMedida
+console.log(peso)
