61468b0163af081f8e6a1e2f8cb39ca1bd475290
wagner-rodri <luan.frra@gmail.com>
2020-08-05 21:24:40 -0300

    Feitos exercícios simples em javascript

---
+++ exercicios/variaveis/js/script.js
@@ -0,0 +1,20 @@
+//Criação de variáveis
+var nome = 'Wagner';
+var idade = 28;
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade)
+
+//Efetuando contas simples com variáveis
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = preco * totalComprado;
+console.log(totalPreco);
+
+//Inserindo variáveis sem repetir a palavra "var"
+var nome = 'Luan', 
+    cidade = 'Curitiba';
+console.log(nome, cidade)
+
+//Declarando variáveis sem valor
+var semValor;
+console.log(semValor) // Deve aparecer "undefined"
