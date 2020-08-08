40d26e448d8b7ef3ab1b7c01969e159738a88ad9
wagner-rodri <luan.frra@gmail.com>
2020-08-08 00:35:29 -0300

    aula0209-atribuicao-e-ternario

---
+++ exercicios/aula0209-atribuicao-e-ternario/js/js/index.js
@@ -0,0 +1,10 @@
+//1. Some 500 ao valor de scroll abaixo atribuindo um novo valor a scroll
+var scroll = 1000
+scroll += 500
+console.log(scroll)
+
+//2. Atribua true para a variável darCredito, caso o cliente possua carro e casa, e falsa caso contrário
+var possuiCarro = true
+var possuiCasa = true
+var darCredito = (possuiCarro == true && possuiCasa == true) ? true : false
+console.log (darCredito)
