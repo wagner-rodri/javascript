a6ad35b667aa1351e430ffc3cedebc1e3495d47c
wagner-rodri <luan.frra@gmail.com>
2020-08-20 20:22:37 -0300

    Finalizado exercicio de verificador de idade

---
+++ exercicios/curso-em-video/verificador-de-idade/README.md
@@ -0,0 +1,2 @@
+# javascript
+Exercício onde, ao inserir o sexo e a idade do usuário, o site mostra uma imagem, meramente ilustrativa, de uma pessoa do mesmo sexo e faixa etária do usuário.
---
+++ exercicios/curso-em-video/verificador-de-idade/script.js
@@ -0,0 +1,69 @@
+var botao = document.querySelector('#botao')
+botao.addEventListener('click',verificar)
+
+var data = new Date()
+var anoAtual = data.getFullYear()
+var res = document.querySelector('div#res')
+function verificar() {
+    var anoNascimento = window.document.querySelector('#txtano')
+    //Validação do ano
+    if(anoNascimento.value.length == 0 || anoNascimento.value > anoAtual){
+        window.alert('[ERRO] Verifique as informações e tente novamente!')
+    } else {
+        var sexo = document.getElementsByName('radsex')
+        var idade = anoAtual - Number(anoNascimento.value)
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (sexo[0].checked) {
+            var genero = 'Homem'
+            if (idade >=0 && idade <10) {
+                //criança
+                img.setAttribute('src', 'img/crianca-menino.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'img/jovem-menino.png')
+            } else if (idade <60){
+                //Adulto
+                img.setAttribute('src','img/adulto-homem.png')
+            } else {
+                //idoso
+                img.setAttribute('src','img/idoso.png')
+            }
+        } else {
+            genero = 'Mulher'
+            if (idade >=0 && idade <10) {
+                //criança
+                img.setAttribute('src','img/crianca-menina.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src','img/jovem-menina.png')
+            } else if (idade <65){
+                //Adulto
+                img.setAttribute('src','img/adulto-mulher.png')
+            } else {
+                //idoso
+                img.setAttribute('src','img/idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos de idade`
+        res.appendChild(img)
+    }
+
+}
+
+
+/*
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear() 
+    var fano = window.document.getElementById('txtano')
+    var res = document.querySelector('div#res')    
+    //Verificação do ano
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert('[ERRO] Verifique as informações e tente novamente!')
+    }
+
+
+}
+*/
