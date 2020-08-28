be109c39ead9caa5e69b62f8b30bcbfb18a92269
wagner-rodri <luan.frra@gmail.com>
2020-08-28 11:08:47 -0300

    Tarefa #1
    Tarefa onde pude aplicar os Data Types e operadores.

---
+++ exercicios/udemy-academind-schwarzmüller/basics-assignment-1-start/assignment.js
@@ -0,0 +1,21 @@
+
+//Step #1
+let userInput = 10;
+let result = '';
+
+//Step #2
+result = userInput + 18;
+
+//Step #3
+
+result = result - 8;
+result = result * 3;
+result = result / 3;
+
+//Step #4
+
+//No, it didn't change
+
+//Step #5
+alert(`The user input is ${userInput}`)
+alert(`The result is ${result}`)
