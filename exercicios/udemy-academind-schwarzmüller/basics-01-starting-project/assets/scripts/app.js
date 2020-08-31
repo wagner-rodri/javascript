5b03cb490b49a748adbcd449f03e6809ca473174
wagner-rodri <luan.frra@gmail.com>
2020-08-31 11:36:31 -0300

    Inserida demais funções da calculadora
    Codificado código do arquivo app.js.
    
    - Utilizado funções dentro de funções.
    - Add comentários para facilitar a compreensão do código.

--- exercicios/udemy-academind-schwarzmüller/basics-01-starting-project/assets/scripts/app.js
+++ exercicios/udemy-academind-schwarzmüller/basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,49 @@
 const defaultResult = 0;
-const currentResult = defaultResult;
+let currentResult = defaultResult;
 
-function add() {
-    currentResult += userInput.value;
-    outputResult(currentResult,calculationDescription)
+//Gets input from input field
+function getUserNumberInput() {
+    return parseInt(userInput.value)
 }
 
-addBtn.addEventListener('click', add)
+//Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) { //Parameters provided from Mathematical functions below
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription); //From vendor.js file
+}
+
+//Mathematical functions
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber)
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber)
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult
+    currentResult += enteredNumber;
+    createAndWriteOutput('x', initialResult, enteredNumber)
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber)
+}
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
