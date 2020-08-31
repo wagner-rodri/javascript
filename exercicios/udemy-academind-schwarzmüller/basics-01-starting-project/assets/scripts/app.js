3aa62f313b4d5d8c48489a107a3396c3f2a9ed16
wagner-rodri <luan.frra@gmail.com>
2020-08-31 20:20:21 -0300

    Atualizado exercício
    Geradas funções onde são cirados objetos com dados importantes de cada cálculo efetuado com a calculadora.

--- exercicios/udemy-academind-schwarzmüller/basics-01-starting-project/assets/scripts/app.js
+++ exercicios/udemy-academind-schwarzmüller/basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,10 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 //Gets input from input field
 function getUserNumberInput() {
-    return parseInt(userInput.value)
+    return parseInt(userInput.value);
 }
 
 //Generates and writes calculation log
@@ -12,33 +13,54 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) { //Parame
     outputResult(currentResult, calcDescription); //From vendor.js file
 }
 
+//Creates new objects for each operation, then adds them into array logEntries
+function writeToLog(
+    operationIdentifier,
+    prevResult,
+    operationNumber,
+    newResult
+) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 //Mathematical functions
 function add() {
     const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult
+    const initialResult = currentResult;
     currentResult += enteredNumber;
-    createAndWriteOutput('+', initialResult, enteredNumber)
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    writeToLog('ADD', initialResult, enteredNumber,currentResult)
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult
+    const initialResult = currentResult;
     currentResult -= enteredNumber;
-    createAndWriteOutput('-', initialResult, enteredNumber)
+    createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('SUBTRACT', initialResult, enteredNumber,currentResult)
 }
 
 function multiply() {
     const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult
-    currentResult += enteredNumber;
-    createAndWriteOutput('x', initialResult, enteredNumber)
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('x', initialResult, enteredNumber);
+    writeToLog('MULTIPLY', initialResult, enteredNumber,currentResult)
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult
+    const initialResult = currentResult;
     currentResult /= enteredNumber;
-    createAndWriteOutput('/', initialResult, enteredNumber)
+    createAndWriteOutput('/', initialResult, enteredNumber);
+    writeToLog('DIVIDE', initialResult, enteredNumber,currentResult)
 }
 
 
