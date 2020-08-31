6e07356706696bf5b9cf5b084a18b4875851b2c1
wagner-rodri <luan.frra@gmail.com>
2020-08-31 10:40:13 -0300

    Updated exercise

--- exercicios/udemy-academind-schwarzmüller/basics-assignment-2-start/assignment.js
+++ exercicios/udemy-academind-schwarzmüller/basics-assignment-2-start/assignment.js
@@ -14,7 +14,10 @@ nameParameter('Wagner')
 task3Element.addEventListener('click',noParameter)
 
 function task_4 (txt1, txt2, txt3) {
-    alert(`${txt1}! ${txt2} ${txt3}!`)
+    const combinedText = `${txt1}! ${txt2} ${txt3}!`
+    return combinedText
 }
 
-task_4('Hello', 'It', 'works')
+const combinedString = task_4('Hello', 'It', 'works')
+alert(combinedString)
+
