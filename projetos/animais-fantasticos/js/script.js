ba904f807afc9611f5a2a91531c199a59b65632a
wagner-rodri <luan.frra@gmail.com>
2020-09-01 19:09:30 -0300

    Codificado scroll suave
    Codificado com
    window.scrollTo
    scrollIntoView
    Mas sem sucesso.
    Resta correção.

--- projetos/animais-fantasticos/js/script.js
+++ projetos/animais-fantasticos/js/script.js
@@ -42,3 +42,31 @@ function faqList () {
     }
 }
 faqList();
+
+
+function scrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        //event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+        /*     
+        Usando o window.scrollTo
+        const topo = section.offSetTop;
+        window.scrollTo({
+            top: topo,
+            behavior: 'smooth',
+        }); 
+        */
+    }
+    linksInternos.forEach ((link) => {
+        link.addEventListener('click',scrollToSection);
+    })
+}
+scrollSuave();
