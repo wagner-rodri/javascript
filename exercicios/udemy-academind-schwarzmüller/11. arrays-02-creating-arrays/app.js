c6e4462fb4500b69e9faafdb8b707d8b64d42e16
wagner-rodri <luan.frra@gmail.com>
2020-09-11 18:12:02 -0300

    Anotações sobre manipulação de arrays

--- exercicios/udemy-academind-schwarzmüller/11. arrays-02-creating-arrays/app.js
+++ exercicios/udemy-academind-schwarzmüller/11. arrays-02-creating-arrays/app.js
@@ -21,3 +21,63 @@
 // console.log(hobbies)
 // hobbies.splice(-3, 2)
 // console.log(hobbies)
+
+
+// const personData = [{name: 'manuel'}, {name: 'ricardo'}]
+// const person = personData.find((person, id, persons) => {
+//     return person.name === 'manuel';
+// });
+// console.log(person)
+
+
+// const prices = [10.99, 5, 6.85];
+// const tax = 0.105;
+// const newPrices = [];
+
+// prices.forEach((price, idx) => {
+//     const priceObj = {index: idx, newPrice: price + (price * tax)};
+//     newPrices.push(priceObj);
+// });
+
+// console.log(newPrices)
+
+
+// const numeros = [1, 3, 5, 8, 7, 9];
+// const vetor = [];
+
+// numeros.forEach((num, idx) => {
+//     const soma = 10;
+//     const numObj = {
+//         index: idx, 
+//         numeroAntigo: num, 
+//         numeroSomado: soma,
+//         numeroAposSoma: num + soma
+//     }
+//     vetor.push(numObj)
+// });
+
+// console.log(vetor);
+
+// const numeros = [1, 3, 5, 8, 7, 9];
+// const vetor = [];
+
+// numeros.forEach((num, idx) => {
+//     const soma = 10;
+//     const numObj = {
+//         index: idx, 
+//         numeroAntigo: num, 
+//         numeroSomado: soma,
+//         numeroAposSoma: num + soma
+//     }
+//     vetor.push(numObj)
+// });
+
+// console.log(vetor);
+
+// const sum = numeros.reduce((prevValue, curValue) => prevValue + curValue, 0)
+// console.log(sum)
+
+// const data = 'new york;19.99;2000'
+
+// const transformedData = data.split(';')[2];
+// console.log(transformedData)
