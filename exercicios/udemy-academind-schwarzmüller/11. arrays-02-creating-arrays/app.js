35f9aa856e9f4fcd5e4d486bfb7b928333be67c8
wagner-rodri <luan.frra@gmail.com>
2020-09-11 12:10:20 -0300

    Atualizado nome das pastas

---
+++ exercicios/udemy-academind-schwarzmüller/11. arrays-02-creating-arrays/app.js
@@ -0,0 +1,23 @@
+// const numbers = [1, 2, 3];
+// console.log(numbers);
+
+// // const moreNumbers = Array(5, 2);
+// // console.log(moreNumbers);
+
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
+
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
+
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
+
+
+// const hobbies = ['coding', 'running'];
+// hobbies.push('cooking');
+// hobbies.pop();
+// hobbies.splice(2, 0, 'dancing', 'eating')
+// console.log(hobbies)
+// hobbies.splice(-3, 2)
+// console.log(hobbies)
