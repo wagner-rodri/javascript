5b9d9159fc6c98f703e784ac33cbe707467d0ada
wagner-rodri <luan.frra@gmail.com>
2020-09-16 15:25:33 -0300

    Função render( ) colocada dentro de uma Class, transformando-a em Method

--- exercicios/udemy-academind-schwarzmüller/14. oop-01-starting-setup/assets/scripts/app.js
+++ exercicios/udemy-academind-schwarzmüller/14. oop-01-starting-setup/assets/scripts/app.js
@@ -1,3 +1,26 @@
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+
+    render() {
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
+                <div>
+                    <img src="${this.product.imageUrl}" alt="${this.product.title}">
+                    <div class="product-item__content">
+                        <h2>${this.product.title}</h2>
+                        <h3>\$${this.product.price}</h3>
+                        <p>${this.product.description}</p>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            `;
+        return prodEl;
+    }
+}
+
 class Product {
     title = 'DEFAULT';
     imageUrl;
@@ -12,9 +35,8 @@ class Product {
     }
 }
 
-
-const productList = {
-    products: [
+class ProductList {
+    products = [
         new Product(
             'A Pillow', 
             'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Pillows_on_a_hotel_bed.jpg/220px-Pillows_on_a_hotel_bed.jpg', 
@@ -27,29 +49,22 @@ const productList = {
                 'A carpet you might like!',
                 59.99
             )
-    ],
+    ];
+
+    constructor() {};
+
     render() {
         const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-                <div>
-                    <img src="${prod.imageUrl}" alt="${prod.title}">
-                    <div class="product-item__content">
-                        <h2>${prod.title}</h2>
-                        <h3>\$${prod.price}</h3>
-                        <p>${prod.description}</p>
-                        <button>Add to Cart</button>
-                    </div>
-                </div>
-            `;
+            const productItem = new ProductItem(prod)
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
     }
-};
+}
 
-productList.render();
+const produList = new ProductList ();
+produList.render();
