7b0ad81aadb4346d476aa891d5ebb6abe4d65db5
wagner-rodri <luan.frra@gmail.com>
2020-09-29 21:09:27 -0300

    Adionado dados ao Local Storage
    Adicionado filme e assentos selecionados.

--- exercicios/udemy-vanilla-javascript/movie-seat-booking/script.js
+++ exercicios/udemy-vanilla-javascript/movie-seat-booking/script.js
@@ -1,13 +1,23 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 
+// Save selected Movie and Price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // Update total and Count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+        const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount*ticketPrice;
@@ -16,6 +26,7 @@ function updateSelectedCount() {
 // Movie Select Event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
