28382c24a305afdf9ec57ac318bed6817969423e
wagner-rodri <luan.frra@gmail.com>
2020-10-13 20:57:05 -0300

    Implementado Inheritance
    Adicionado Inheritance à ShoppingCart.
    
    Resta adicionar às demais Classes.

--- exercicios/udemy-academind-schwarzmüller/14. oop-01-starting-setup/assets/scripts/app.js
+++ exercicios/udemy-academind-schwarzmüller/14. oop-01-starting-setup/assets/scripts/app.js
@@ -12,23 +12,64 @@ class Product {
     }
 }
 
-class ShoppingCart {
+class ElementAttribute {
+    constructor(attrName, attrValue) {
+        this.name = attrName;
+        this.value = attrValue;
+    }
+}
+
+class Component {
+    constructor(renderHookId) {
+        this.hookId = renderHookId;
+    }
+
+    createRouteElement(tag, cssClasses, attributes) {
+        const rootElement = document.createElement(tag);
+        if (cssClasses) {
+            rootElement.className = cssClasses;
+        }
+        if (attributes && attributes.length > 0) {
+            for (const attr of attributes) {
+                rootElement.setAttribute(attr.name, attr.value)
+            }
+        }
+        document.getElementById(this.hookId).append(rootElement);
+        return rootElement;
+    }
+}
+
+class ShoppingCart extends Component {
     items = [];
 
+    set cartItems(value) {
+        this.items = value;
+        this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+    }
+
+    get totalAmount() {
+        const sum = this.items.reduce(
+            (prevValue, curItem) => prevValue + curItem.price, 0);
+        return sum;
+    }
+
+    constructor(renderHookId) {
+        super(renderHookId);
+    }
+
     addProduct(product) {
-        this.items.push(product);
-        this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+        const updatedItems = [...this.items];
+        updatedItems.push(product);
+        this.cartItems = updatedItems;
     }
 
     render () {
-        const cartEl = document.createElement('section');
+        const cartEl = this.createRouteElement('section', 'cart');
         cartEl.innerHTML = `
             <h2>Total: \$${0}</h2>
             <button>Order Now!</button>
         `;
-        cartEl.className = 'cart';
         this.totalOutput = cartEl.querySelector('h2');
-        return cartEl
     }
 }
 
@@ -38,7 +79,7 @@ class ProductItem {
     }
 
     addToCart () {
-        App.addProductToCart(this.product);
+        Page.addProductToCart(this.product);
     }
 
     render() {
@@ -97,17 +138,16 @@ class Shop {
     render() {
         const renderHook = document.getElementById('app');
         
-        this.cart = new ShoppingCart();
-        const cartEl = this.cart.render();
+        this.cart = new ShoppingCart('app');
+        this.cart.render();
         const productList = new ProductList();
         const prodListEl = productList.render();
         
-        renderHook.append(cartEl);
         renderHook.append(prodListEl);
     }
 }
 
-class App {
+class Page {
     static cart;
 
     static init() {
@@ -121,4 +161,4 @@ class App {
     }
 }
 
-App.init();
+Page.init();
