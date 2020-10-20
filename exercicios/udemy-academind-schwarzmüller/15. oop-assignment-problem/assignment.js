53ab992f4cef484b289e7338998f347c10c038aa
wagner-rodri <luan.frra@gmail.com>
2020-10-20 15:32:09 -0300

    Excluida parte que já esva escrita no arquivo index.html

--- exercicios/udemy-academind-schwarzmüller/15. oop-assignment-problem/assignment.js
+++ exercicios/udemy-academind-schwarzmüller/15. oop-assignment-problem/assignment.js
@@ -1,38 +1,3 @@
-
-// Create a "Course" class which has three properties: "title", "length"
-// and "price" (all set up in the constructor). Instantiate the class twice
-// (two dummy courses of your choice). Output the objects to the console.
-
-// Add two methods to the "Course" class: A method which calculates the
-// length/price value ("how much length do you get for the amount paid")
-// and one method that outputs a nice course summary (including title,
-// length and price). Call these methods thereafter (and output the result
-// to the console).
-
-// class TheoreticalCourse extends Course {
-//     publish() {
-
-//     }
-// }
-
-
-// class PracticalCourse extends Courses {
-//     numOfExercises = 10;
-// }
-
-// Create two new, more specialized, classes: "PracticalCourse" and
-// "TheoreticalCourse". Both should be based on the "Course" class but
-// "PracticalCourse" should also have a "numOfExercises" property,
-// "TheoreticalCourse" should have a "publish()" method (which just prints
-// something to the console). Instantiate both and use the new AND old
-// properties and methods.
-
-// Use getters and setters to ensure that the "price" property can only be
-// set to a positive value and is returned with a "$" in front of it.
-
-
-// Use private fields to harden the getter/ setter approach from before.
-        
 class Course {
     #price;
     
