6f613fa761cc7f4adac5746e81845c25e1811476
wagner-rodri <luan.frra@gmail.com>
2020-10-20 20:14:38 -0300

    Realizado testes no arquivo de App.js

--- react-the-complete-guide/src/App.js
+++ react-the-complete-guide/src/App.js
@@ -6,8 +6,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi! I'm a React App</h1>
+        <p>This is really working!</p>
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
